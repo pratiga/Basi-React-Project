c357e6742b0f49f912533aa31492035d78cdbeec
Pratima <gaitrisanu2468@gmail.com>
2022-04-02 21:42:22 +0545

    add tours component details

---
+++ tours/src/Tours.js
@@ -0,0 +1,21 @@
+import Tour from './Tour'
+import React from 'react'
+
+const Tours = ({ tours, removeTour}) => {
+  return (
+    <section>
+      <div className="title">
+      <h2>tours component</h2>
+      <div className='underline'></div>
+      </div>
+      <div>
+       { tours.map((tour) =>{
+         return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>;
+       })}
+      </div>
+    </section>
+  
+  )
+};
+
+export default Tours
