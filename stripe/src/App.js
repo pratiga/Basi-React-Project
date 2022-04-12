193613160910df22fc1721ce0ad2f355f27f4a6d
Pratima <gaitrisanu2468@gmail.com>
2022-04-12 22:12:24 +0545

    context page
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

---
+++ stripe/src/App.js
@@ -0,0 +1,19 @@
+import { Hero } from "./Hero";
+import { Navbar } from "./Navbar";
+import { Sidebar } from "./Sidebar";
+
+
+function App() {
+  return(
+      <>
+    <Navbar />
+    <Sidebar />
+    <Hero />
+   
+    
+    
+    </>
+  )
+}
+
+export default App;
