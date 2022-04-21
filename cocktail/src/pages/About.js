0e2466ea4a0ea5e3be1cced927404d862ed18a57
Pratima <gaitrisanu2468@gmail.com>
2022-04-21 16:41:44 +0545

    add about page
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

---
+++ cocktail/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+    <h1 className='section-title'>about us</h1>
+    <p>
+    Cinnamon is a sweet, fragrant spice produced from the inner bark of trees of the genus Cinnamomum, which is native to the Indian sub-continent. It was known in biblical times, and is mentioned
+     in several books of the Bible, both as an
+     ingredient that was mixed with oils for anointing people’s bodies,
+     and also as a token indicating friendship among lovers and friends. In ancient Rome, mourners attending funerals burnt cinnamon to create a pleasant scent. Most often, however, the spice found its primary use as an additive to food and drink. In the Middle Ages,
+     Europeans who could afford the spice used it to flavor food,
+     particularly meat,
+    and to impress those around them with their ability to purchase 
+    </p>
+    </section>
+  )
+}
+
+export default About
